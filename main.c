ad9bcb57b5793a02f3da176301f442571ca87f6c
alexandr.pomortsev <a-pomor@mail.ru>
2021-05-20 19:15:58 +0300

    update file

--- main.c
+++ main.c
@@ -44,6 +44,25 @@ int exponent2(int num, int exp) {
 	return result;
 }
 
+//3. Задание
+int routes(int x, int y, int letX, int letY) {
+	if((x == 0) && (y == 0)) {
+		return 0;
+	}
+	else if((x == letX) && (y == letY)) {
+		return 0;
+	}
+	else if((x == 0) || (y == 0)) {
+		return 1;
+	}
+	else if((x == letX - 1) || (y == letY - 1)) {
+		return 1;
+	}
+	else {
+		return routes(x, y - 1, letX, letY) + routes(x - 1, y, letX, letY);
+	}
+}
+
 int main()
 {
 	// 1. Реализовать функцию перевода чисел из десятичной системы в двоичную, используя рекурсию.
@@ -68,7 +87,18 @@ int main()
 	printf("Возведение числа %d в степень %d = %d\n", num_a, exp_b, exponent2(num_a,exp_b));
 	
 	// 3. Реализовать нахождение количества маршрутов шахматного короля с препятствиями (где единица - это наличие препятствия, а ноль - свободная для хода клетка), при этом король всегда начинает в координатах (0,0), и может ходить только вправо и вниз.
-	//*в процессе написания
+	const int size_X = 4;
+	const int size_Y = 4;
+	
+	int letX = 1;
+	int letY = 1;
+	
+	for(int y = 0; y < size_Y; y++) {
+		for(int x = 0; x < size_X; x++) {
+			printf("%4d", routes(x, y, letX, letY));
+		}
+		printf("\n");
+	}
 	
 	return 0;
 }
